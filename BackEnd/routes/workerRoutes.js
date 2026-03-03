4040ab7a463dfcf7644e0e27076259a237f7941b
Lubna <lubnaturkmany@gmail.com>
2026-03-03 17:15:02 +0300

    add (delete worker)

--- BackEnd/routes/workerRoutes.js
+++ BackEnd/routes/workerRoutes.js
@@ -1,15 +1,26 @@
 const express = require("express"); 
 const router = express.Router(); 
+const { protect, authorizeRoles } = require("../middleware/authMiddleware");
 const { 
     addWorker, 
     getAllWorkers, 
-    getWorkerByID 
+    getWorkerByID,
+    deleteWorkerByID,
+    deleteAllWorkers 
       } = require("../controllers/workerControllers"); 
-// إضافة عامل 
-router.post("/workers", addWorker); 
-// عرض كل العمال 
- router.get("/workers", getAllWorkers);
-// عرض عامل معيّن 
-router.get("/workers/:workerID", getWorkerByID);
+// إضافة عامل → Admin فقط
+router.post("/", protect, authorizeRoles("Admin"), addWorker);
+
+// عرض كل العمال → Admin + Supervisor
+router.get("/", protect, authorizeRoles("Admin", "Supervisor"), getAllWorkers);
+
+// عرض عامل معين → Admin + Supervisor
+router.get("/:workerID", protect, authorizeRoles("Admin", "Supervisor"), getWorkerByID);
+
+// حذف عامل معين → Admin فقط
+router.delete("/:workerID", protect, authorizeRoles("Admin"), deleteWorkerByID);
+
+// حذف جميع العمال → Admin فقط
+router.delete("/", protect, authorizeRoles("Admin"), deleteAllWorkers);
 
 module.exports = router;
